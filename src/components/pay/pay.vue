f8ded4f819bcde8392dc0a6fd6e9d338ba33bbd9
neusc <neusc829@163.com>
2017-03-03 14:48:03 +0800

    update pay template

--- src/components/pay/pay.vue
+++ src/components/pay/pay.vue
@@ -1,7 +1,31 @@
 <template>
   <transition name="move">
     <div v-show="showFlag" class="pay-wrapper" ref="pay">
-      总共支付{{totalPrice}}元！
+      <div class="pay-way">
+        <div class="pay-title">选择付款方式</div>
+        <label for="ali-check-btn" class="alipay-wrapper pay-option" data-checked="true">
+          <img class="alipay-icon" src="http://p0.jmstatic.com/mobile/touch/Image/duobao/zhenl3/alipay-icon.png"/>
+          <div class="alipay-des-wrapper">
+            <div class="alipay-des-top">支付宝</div>
+            <div class="alipay-des-bottom">客户端支付最便捷！可银行卡支付</div>
+          </div>
+          <div class="check-btn-wrapper">
+            <input type="radio" id="ali-check-btn" class="check-btn" name="check-btn" value="alipay" checked/>
+            <label class="check-btn-label" for="ali-check-btn"></label>
+          </div>
+        </label>
+        <label for="wx-check-btn" class="wxpay-wrapper pay-option">
+          <img class="wxpay-icon" src="http://p0.jmstatic.com/mobile/touch/Image/duobao/zhenl3/wxpay-icon.png"/>
+          <div class="wxpay-des-wrapper">
+            <div class="wxpay-des-top">微信支付</div>
+            <div class="wxpay-des-bottom">亿万用户的选择，更快更安全</div>
+          </div>
+          <div class="check-btn-wrapper">
+            <input type="radio" id="wx-check-btn" class="check-btn" name="check-btn" value="wxpay"/>
+            <label class="check-btn-label" for="wx-check-btn"></label>
+          </div>
+        </label>
+      </div>
     </div>
   </transition>
 </template>
